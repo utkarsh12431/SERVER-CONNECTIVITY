66b21431b502cd798cf2c21b016cbcbafb8f4532
utkarsh12431 <utkarshsinhaz2010@gmail.com>
2026-03-08 15:35:12 +0530

    Create connection file
    Connection file to link between database and web page.

--- connection.php
+++ connection.php
@@ -5,6 +5,7 @@
     $password = "";
     $dbname = "";
 //insert your servername, username ,password and database name in which you want to enter your data.
+// for localhost, use servername as localhost, username as root, password will be nill and add your created database name. 
     $conn = mysqli_connect($servername,$username,$password,$dbname);
 
     if($conn)
